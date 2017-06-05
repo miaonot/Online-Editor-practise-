f84889ef9348b287d44de06fe71d6efac2ea78bf
miaonot <miaonot@gmail.com>
2017-06-05 22:20:38 +0800

    v0.0.2 注册功能完成

--- src/routes.php
+++ src/routes.php
@@ -9,10 +9,47 @@
 //    return $this->renderer->render($response, 'index.phtml', $args);
 //});
 
+session_start();
+
 $app->get('/test', function ($request, $response, $args) {
-    return $this->renderer->render($response, 'test.phtml');
+    //return $this->renderer->render($response, 'test.phtml');
+    echo "test";
 });
 
 $app->get('/join', function ($request, $response, $args) {
-    return $this->renderer->render($response, 'join.phtml');
+    if ($_SESSION['username'] != null) {
+        //TODO: 改用重定向，而不是直接显示
+        return $this->renderer->render($response, 'index.phtml');
+    } else {
+        return $this->renderer->render($response, 'join.phtml');
+    }
+});
+
+$app->get('/login', function ($request, $response, $args) {
+    if ($_SESSION['username'] != null) {
+        return $this->renderer->render($response, 'index.phtml');
+    } else {
+        return $this->renderer->render($response, 'login.phtml');
+    }
+});
+
+require __DIR__ . '/../src/models/User.models.php';
+
+$app->post('/join', function ($request, $response, $args) {
+    $user = User::where('name', '=', $_POST['username'])->first();
+    if ($user->name != null) {
+        echo "error";
+    } else {
+//        $new_user = new User;
+//        $new_user->name = $_POST['username'];
+//        $new_user->password = substr(crypt($_POST['password'], '$6$what ever salt it is'), 20);
+//        $new_user->save();
+//        $_COOKIE['username']=$_POST['username'];
+        setcookie("username", $_POST['username'], 3600);
+        echo "success";
+    }
+});
+
+$app->get('/index', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'index.phtml');
 });
