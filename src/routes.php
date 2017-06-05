c7916b77d4f62277aaf849e6e998ac7de770750f
miaonot <miaonot@gmail.com>
2017-06-05 23:53:07 +0800

    v0.0.3 登陆、主页完成

--- src/routes.php
+++ src/routes.php
@@ -17,23 +17,31 @@ $app->get('/test', function ($request, $response, $args) {
 });
 
 $app->get('/join', function ($request, $response, $args) {
-    if ($_SESSION['username'] != null) {
-        //TODO: 改用重定向，而不是直接显示
-        return $this->renderer->render($response, 'index.phtml');
+    if ($_COOKIE['username'] != null) {
+        return $response->withStatus(200)->withHeader('Location', '/diary/public/index');
     } else {
         return $this->renderer->render($response, 'join.phtml');
     }
 });
 
 $app->get('/login', function ($request, $response, $args) {
-    if ($_SESSION['username'] != null) {
-        return $this->renderer->render($response, 'index.phtml');
+    if ($_COOKIE['username'] != null) {
+        return $response->withStatus(200)->withHeader('Location', '/diary/public/index');
     } else {
         return $this->renderer->render($response, 'login.phtml');
     }
 });
 
 require __DIR__ . '/../src/models/User.models.php';
+
+$app->post('/login', function ($request, $response, $args) {
+    $user = User::where('name', '=', $_POST['username'])->first();
+    if ($user->name != null && $user->password === substr(crypt($_POST['password'], '$6$what ever salt it is'), 20)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+});
 
 $app->post('/join', function ($request, $response, $args) {
     $user = User::where('name', '=', $_POST['username'])->first();
@@ -45,11 +53,16 @@ $app->post('/join', function ($request, $response, $args) {
 //        $new_user->password = substr(crypt($_POST['password'], '$6$what ever salt it is'), 20);
 //        $new_user->save();
 //        $_COOKIE['username']=$_POST['username'];
-        setcookie("username", $_POST['username'], 3600);
+        setcookie("username", $_POST['username'], -1);
         echo "success";
     }
 });
 
 $app->get('/index', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'index.phtml');
+});
+
+$app->get('/logout', function ($request, $response, $args) {
+    setcookie('username', "", 0);
     return $this->renderer->render($response, 'index.phtml');
 });
